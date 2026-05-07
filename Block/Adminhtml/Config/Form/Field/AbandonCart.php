7f9c81ca3212d05d977b61d8adb7e907e225e675
zubairazguards <zubair.s@azguardstech.com>
2026-05-07 20:00:08 +0530

    Order shipped send template

---
+++ Block/Adminhtml/Config/Form/Field/AbandonCart.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Azguards\WhatsAppConnect\Block\Adminhtml\Config\Form\Field;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\DataObject;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Azguards\WhatsAppConnect\Helper\ApiHelper;
+
+/**
+ * @method AbstractElement getElement()
+ * @method $this setElement(AbstractElement $element)
+ */
+class AbandonCart extends Field
+{
+    public const XML_PATH_ABANDON_CART =
+    "whatsApp_conector/abandon_cart/abandoned_cart_variable";
+    
+    /**
+     * @var ApiHelper
+     */
+    public $helper;
+
+    /**
+     * Abandon Cart
+     *
+     * @param Context $context
+     * @param ApiHelper $helper
+     */
+    public function __construct(
+        Context $context,
+        ApiHelper $helper
+    ) {
+        parent::__construct($context);
+        $this->helper = $helper;
+    }
+
+    /**
+     * Construct
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->setTemplate(
+            'Azguards_WhatsAppConnect::config/form/field/indices.phtml'
+        );
+    }
+    /**
+     * Get Dropdown Options
+     *
+     * @return void
+     */
+    public function getDropdownOptions()
+    {
+        return [
+            // Quote (Magento) fields and getters used by TemplateVariableResolver.
+            'entity_id' => __('Cart ID'),
+            'created_at' => __('Cart Created At'),
+            'updated_at' => __('Cart Updated At'),
+            'grand_total' => __('Cart Grand Total'),
+            'subtotal' => __('Cart Subtotal'),
+            'items_count' => __('Cart Items Count'),
+            'items_qty' => __('Cart Items Quantity'),
+            'coupon_code' => __('Cart Coupon Code'),
+            'customer_email' => __('Cart Customer Email'),
+            'customer_firstname' => __('Cart Customer First Name'),
+            'customer_lastname' => __('Cart Customer Last Name'),
+            'customer_is_guest' => __('Is Guest Cart'),
+            'is_active' => __('Cart Is Active'),
+        ];
+    }
+    /**
+     * Render
+     *
+     * @param AbstractElement $element
+     * @return void
+     */
+    public function render(AbstractElement $element)
+    {
+        $this->setElement($element);
+
+        return $this->_toHtml();
+    }
+
+    /**
+     * Available indexes
+     *
+     * @param array|string|int $option
+     * @return void
+     */
+    public function getOptionData($option)
+    {
+        if (!empty($option)) {
+            return $option;
+        }
+        // Fetch the stored configuration data
+        $userRegistrationData = $this->helper->getConfigValue(self::XML_PATH_ABANDON_CART);
+        $decodedData = (!empty($userRegistrationData) && is_string($userRegistrationData)) ?
+         json_decode($userRegistrationData, true) : [];
+        foreach ($decodedData as &$index) {
+            foreach (['title', 'order', 'limit', 'type', 'identifier'] as $key) {
+                if (isset($index[$key]) && is_string($index[$key])) {
+                    $index[$key] = str_replace('"', '', $index[$key]); // Remove double quotes
+                }
+            }
+        }
+        if (empty($decodedData)) {
+            return [];
+        }
+        return $decodedData;
+    }
+
+    /**
+     * Index name
+     *
+     * @param IndexInterface $index
+     * @return string
+     */
+    public function getNamePrefix($index)
+    {
+        $element = $this->getElement();
+        if (!$element) {
+            return 'groups[abandon_cart][fields][abandoned_cart_variable][value][' .
+            $index['identifier'] . ']';
+        }
+        return $element->getName() . '[' . $index['identifier'] . ']';
+    }
+
+    /**
+     * Get Value
+     *
+     * @param IndexInterface $index
+     * @param string $item
+     * @return string
+     */
+    public function getValue($index, $item)
+    {
+        $identifier = $index->getIdentifier();
+            $values = $this->getElement()->getData('value');
+        if (isset($values[$identifier]) && isset($values[$identifier][$item])) {
+            return $values[$identifier][$item];
+        }
+
+        return false;
+    }
+}
